19382f1b0db2c7dd805f39c2ca5540c440e38a1e
lukasgartmair <lukas_gartmair@gmx.de>
2016-11-15 11:00:39 +0100

    intermediate

--- reference_data_hull.py
+++ reference_data_hull.py
@@ -12,6 +12,11 @@ from mpl_toolkits.mplot3d import axes3d
 from scipy.spatial import ConvexHull, Delaunay
 from matplotlib.path import Path
 
+def in_sphere(center, radius, point):
+    square_dist = ((center[0] - point[0]) ** 2 + (center[1] - point[1]) ** 2 + (center[2] - point[2]) ** 2 )
+    return square_dist <= radius ** 2
+
+
 def in_hull(p, hull):
     #http://stackoverflow.com/questions/16750618/whats-an-efficient-way-to-find-if-a-point-lies-in-the-convex-hull-of-a-point-cl
     """
@@ -27,11 +32,11 @@ def in_hull(p, hull):
 
     return hull.find_simplex(p)>=0
         
-def generate_rnd_spatial_points(n,hull_minbound, hull_maxbound):
+def generate_rnd_spatial_points(n, minbound, maxbound):
 
-    x_rnd = np.random.uniform(hull_minbound[0],hull_maxbound[0],n)
-    y_rnd = np.random.uniform(hull_minbound[1],hull_maxbound[1],n)
-    z_rnd = np.random.uniform(hull_minbound[2],hull_maxbound[2],n)
+    x_rnd = np.random.uniform(minbound[0],maxbound[0],n)
+    y_rnd = np.random.uniform(minbound[1],maxbound[1],n)
+    z_rnd = np.random.uniform(minbound[2],maxbound[2],n)
 
     rnd_points = np.zeros((n,3))
     rnd_points[:,0] = x_rnd
@@ -67,14 +72,48 @@ inside = np.where(in_hull(rnd_points, convex_hull))
 
 posfile = rnd_points[inside]
 
-ax.scatter(posfile[:,0], posfile[:,1], posfile[:,2])
-for simplex in convex_hull.simplices:
-    ax.plot(points[simplex, 0], points[simplex, 1],points[simplex, 2])
-
-pl.show()
 
 mass_to_charge_matrix = 0
 mass_to_charge_precipitation = 1
 
 posfile = np.c_[posfile, np.ones(posfile[:,0].size) ]    
 posfile[:,3] = mass_to_charge_matrix
+
+# create spherical precipitations
+sizes_of_precs = [5,10,20]
+number_of_precs = 3
+
+#http://stackoverflow.com/questions/5408276/sampling-uniformly-distributed-random-points-inside-a-spherical-volume
+
+#Generate a set of points uniformly distributed within a cube, then discard the ones whose distance from the center exceeds the radius of the desired sphere.
+#shareeditflag
+center_prec1 = np.array((3,0,0))
+radius_prec1 = 1
+minbound_prec1 = center_prec1 - np.array((radius_prec1,radius_prec1,radius_prec1))
+maxbound_prec1 = center_prec1 + np.array((radius_prec1,radius_prec1,radius_prec1))
+number_of_atoms_prec1 = 300
+prec1_cube = generate_rnd_spatial_points(number_of_atoms_prec1, minbound_prec1, maxbound_prec1)
+spherical_prec = np.where(in_sphere(center_prec1, radius_prec1, prec1_cube))
+prec1_sphere = prec1_cube[spherical_prec]
+
+
+ax.scatter(posfile[:,0], posfile[:,1], posfile[:,2])
+ax.scatter(prec1_sphere[:,0], prec1_sphere[:,1], prec1_sphere[:,2], color='red')
+for simplex in convex_hull.simplices:
+    ax.plot(points[simplex, 0], points[simplex, 1],points[simplex, 2])
+
+pl.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
